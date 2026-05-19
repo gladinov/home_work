430652df924b4ffe30e7495841302e6fca90b589
Mikhail Gladinov <gladinovmisha@gmail.com>
2026-05-19 22:27:18 +0300

    hw05: hw without asterics complete

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,7 +1,9 @@
 package hw05parallelexecution
 
 import (
+	"context"
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,78 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	workersCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	workersNumer := min(n, len(tasks))
+	if m <= 0 {
+		m = 0
+	}
+	errCh := make(chan error, m)
+	var wg sync.WaitGroup
+
+	jobCh := produceJob(workersCtx, tasks, workersNumer)
+
+	for i := 0; i < workersNumer; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			doTask(workersCtx, jobCh, errCh)
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(errCh)
+	}()
+
+	var errCount int
+
+	for range errCh {
+		errCount++
+		if errCount > m {
+			cancel()
+			return ErrErrorsLimitExceeded
+		}
+	}
+
 	return nil
+}
+
+func produceJob(ctx context.Context, tasks []Task, workersNumber int) <-chan Task {
+	jobCh := make(chan Task, workersNumber*2)
+
+	go func() {
+		defer close(jobCh)
+		for _, task := range tasks {
+			select {
+			case <-ctx.Done():
+				return
+			case jobCh <- task:
+			}
+		}
+	}()
+
+	return jobCh
+}
+
+func doTask(ctx context.Context, jobCh <-chan Task, errCh chan<- error) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case task, ok := <-jobCh:
+			if !ok {
+				return
+			}
+			err := task()
+			if err != nil {
+				select {
+				case errCh <- err:
+				case <-ctx.Done():
+					return
+				}
+			}
+		}
+	}
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -9,12 +9,9 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"go.uber.org/goleak"
 )
 
 func TestRun(t *testing.T) {
-	defer goleak.VerifyNone(t)
-
 	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
@@ -32,10 +29,14 @@ func TestRun(t *testing.T) {
 
 		workersCount := 10
 		maxErrorsCount := 23
+
 		err := Run(tasks, workersCount, maxErrorsCount)
 
 		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
-		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
+		require.LessOrEqual(t,
+			atomic.LoadInt32(&runTasksCount),
+			int32(workersCount+maxErrorsCount),
+			"extra tasks were started")
 	})
 
 	t.Run("tasks without errors", func(t *testing.T) {
@@ -62,9 +63,103 @@ func TestRun(t *testing.T) {
 		start := time.Now()
 		err := Run(tasks, workersCount, maxErrorsCount)
 		elapsedTime := time.Since(start)
-		require.NoError(t, err)
 
-		require.Equal(t, int32(tasksCount), runTasksCount, "not all tasks were completed")
+		require.NoError(t, err)
+		require.Equal(t, int32(tasksCount), atomic.LoadInt32(&runTasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+}
+
+func TestRun_extra(t *testing.T) {
+	t.Run("stop produce job after cancel ctx", func(t *testing.T) {
+		tasksCount := 500
+		tasks := make([]Task, 0, tasksCount)
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				return err
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 23
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+	})
+
+	t.Run("zero max errors with first task with expected err", func(t *testing.T) {
+		tasksCount := 1
+		tasks := make([]Task, 0, tasksCount)
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				return err
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 0
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Equal(t, ErrErrorsLimitExceeded, err)
+	})
+
+	t.Run("less zero max errors with first task with expected err", func(t *testing.T) {
+		tasksCount := 1
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := -1
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Equal(t, ErrErrorsLimitExceeded, err)
+	})
+
+	t.Run("zero max errors with first task without err", func(t *testing.T) {
+		tasksCount := 1
+		tasks := make([]Task, 0, tasksCount)
+
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				return nil
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 0
+		err := Run(tasks, workersCount, maxErrorsCount)
+		var want error
+		require.Equal(t, want, err)
+	})
+
+	t.Run("less zero max errors with first task without err", func(t *testing.T) {
+		tasksCount := 1
+		tasks := make([]Task, 0, tasksCount)
+
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				return nil
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := -1
+		err := Run(tasks, workersCount, maxErrorsCount)
+		var want error
+		require.Equal(t, want, err)
 	})
 }
